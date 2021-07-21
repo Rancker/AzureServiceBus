382f203d4be044d088a5c43570f8d54a734548c3
goelchhavi <chhavi.goel@ratesetter.com>
2021-07-21 19:39:30 +0100

    AzureServiceBus - Initial Commit

---
+++ SBReceiver/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using SBShared.Models;
+
+namespace SBReceiver
+{
+    internal class Program
+    {
+        private const string connectionString = "Endpoint=sb://quoteservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9NA5uMoiE6Kb9hwFpfqRk86oBVzPRdRJ/CJujT+To6s=";
+        private const string queueName = "personqueue";
+        private static IQueueClient queueClient;
+
+        private static async Task Main(string[] args)
+        {
+            queueClient = new QueueClient(connectionString, queueName);
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            queueClient.RegisterMessageHandler(ProcessMessageAsync, messageHandlerOptions);
+
+            Console.ReadLine();
+
+            await queueClient.CloseAsync();
+        }
+
+        private static async Task ProcessMessageAsync(Message message, CancellationToken arg2)
+        {
+            var jsonString = Encoding.UTF8.GetString(message.Body);
+            PersonModel person = JsonSerializer.Deserialize<PersonModel>(jsonString);
+            Console.WriteLine($"Person Receiver : {person.FirstName} {person.LastName}");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs arg)
+        {
+            Console.WriteLine($"Message handler exception: {arg.Exception}");
+            return Task.CompletedTask;
+        }
+    }
+}
